97a78f5758ba96fe84831a3da1ac10634c95bb7b
YesicaChui <yesicachuic@gmail.com>
2023-01-23 21:54:02 -0500

    Añadiendo estilos css,clases en html y agregando imagenes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import validator from './validator.js';
 
 function validar() {
   const divResult = document.getElementById("resultado")
-  const valid = validator.isValid(document.getElementById("numeroTarjeta").value)
+  const miInput = document.getElementById("numeroTarjeta")
+  const valid = validator.isValid(miInput.value)
   if (valid) {
     divResult.innerHTML = `<p>Tarjeta valida</p>
-      <p>Compra Exitosa</p>`
+      <p>Compra Exitosa</p>
+      <p>${validator.maskify(miInput.value)}</p>
+      `
+    divResult.style.color="green"
   }
   else {
     divResult.innerHTML = `<p>La Tarjeta es Invalida</p>`
+    divResult.style.color="red"
   }
 }
 const botonFinalizar = document.getElementById("botonFinalizar")
